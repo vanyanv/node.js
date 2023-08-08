681af9082dd2cc4ec64ee56e1c355cd760dbd865
Vardan Vanyan <vardanvanyan@Vardans-MBP.attlocal.net>
2023-08-08 15:15:43 -0700

    added more routes

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,28 @@
 import { Router } from 'express';
 
 // making a new router
-const router = Router()
+const router = Router();
 //router are sub apps, seperate from app, which is global
 
+// product
+router.get('/product', () => {});
+router.get('/product/:id', () => {});
+router.put('/product/:id', () => {});
+router.post('/product/', () => {});
+router.delete('/product/:id', () => {});
 
+//update
 
+router.get('/update', () => {});
+router.get('/update/:id', () => {});
+router.put('/update/:id', () => {});
+router.post('/update/', () => {});
+router.delete('/update/:id', () => {});
 
+//update point
 
+router.get('/updatepoint', () => {});
+router.get('/updatepoint/:id', () => {});
+router.put('/updatepoint/:id', () => {});
+router.post('/updatepoint/', () => {});
+router.delete('/updatepoint/:id', () => {});
