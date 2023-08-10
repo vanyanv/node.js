ad87cc85bc019e9667617e8b7bd326bc7512ef30
Vardan Vanyan <vardan.vanyan@gmail.com>
2023-08-10 12:17:44 -0700

    added cors

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,18 @@ import express from 'express';
 import router from './router';
 //morgan is a logging tool
 import morgan from 'morgan';
+import cors from 'cors';
 //makes the api/server
 const app = express();
 
+//use cors
+app.use(cors());
 //does logging
 app.use(morgan('dev'));
+//allows client to send json
+app.use(express.json());
+//allow client to add query string
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   console.log('hello from express');
