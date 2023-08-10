26a001c6c0e722e66c94ad68dc18bfbf4bff34f6
Vardan Vanyan <vardanvanyan@Vardans-MBP.attlocal.net>
2023-08-10 11:42:02 -0700

    added logging middleware

--- src/router.ts
+++ src/router.ts
@@ -5,14 +5,15 @@ const router = Router();
 //router are sub apps, seperate from app, which is global
 
 // product
-router.get('/product', () => {});
+router.get('/product', (req, res) => {
+  res.json({ message: 'Hello' });
+});
 router.get('/product/:id', () => {});
 router.put('/product/:id', () => {});
 router.post('/product/', () => {});
 router.delete('/product/:id', () => {});
 
 //update
-
 router.get('/update', () => {});
 router.get('/update/:id', () => {});
 router.put('/update/:id', () => {});
@@ -20,9 +21,10 @@ router.post('/update/', () => {});
 router.delete('/update/:id', () => {});
 
 //update point
-
 router.get('/updatepoint', () => {});
 router.get('/updatepoint/:id', () => {});
 router.put('/updatepoint/:id', () => {});
 router.post('/updatepoint/', () => {});
 router.delete('/updatepoint/:id', () => {});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express from 'express';
+import router from './router';
+//morgan is a logging tool
+import morgan from 'morgan';
 //makes the api/server
 const app = express();
+
+//does logging
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   console.log('hello from express');
   res.status(200);
   res.json({ message: 'hello' });
 });
+//use lets you apply a global option
+app.use('/api', router);
 
 export default app;
